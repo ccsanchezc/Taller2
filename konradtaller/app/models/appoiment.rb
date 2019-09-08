1a032cee28c8856274a4427c72b6afe4cd9e8fd5
Camilo sanchez <regimensur01@gmail.com>
2019-09-08 14:45:37 -0400

    validación appoimnet
    validación de citas

--- konradtaller/app/models/appoiment.rb
+++ konradtaller/app/models/appoiment.rb
@@ -1,5 +1,18 @@
 class Appoiment < ApplicationRecord
+
+  validates_presence_of :begindate, :enddate
+  
+  validate :no_reservation_overlap
   belongs_to :consulting_room
   belongs_to :patient
   belongs_to :doctor
+ 
+
+
+
+def no_reservation_overlap
+  if (Appoiment.where("(? BETWEEN begindate AND enddate OR ? BETWEEN begindate AND enddate) AND patient = ? AND doctor = ?", self.begindate, self.enddate, self.patient, self.doctor).any?)
+     errors.add(:enddate, 'it overlaps another reservation')
+  end
+end
 end
